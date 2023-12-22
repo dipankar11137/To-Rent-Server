7f71143104b08a3e47496af8252c978e314da725
dipankar-halder <dipankar15-11137@diu.edu.bd>
2023-12-22 19:38:01 +0600

    review get

--- index.js
+++ index.js
@@ -114,6 +114,13 @@ async function run() {
       const result = await reviewCollection.insertOne(newFlats);
       res.send(result);
     });
+    // get Review
+    app.get('/reviews', async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     //                Book
     // post  book services
     app.post('/bookFlats', async (req, res) => {
